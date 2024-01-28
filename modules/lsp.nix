e1e26fec86e5a68aa5500471d6fa6d2732a162f5
Gytis Ivaskevicius <me@gytis.io>
2024-01-28 04:06:53 +0200

    feat: rebuild lsp config

--- modules/lsp.nix
+++ modules/lsp.nix
@@ -2,6 +2,16 @@
 let
   inherit (lib) getExe;
   capabilities = rawLua "capabilities";
+  border = highlightName: [
+    [ "╭" highlightName ]
+    [ "─" highlightName ]
+    [ "╮" highlightName ]
+    [ "│" highlightName ]
+    [ "╯" highlightName ]
+    [ "─" highlightName ]
+    [ "╰" highlightName ]
+    [ "│" highlightName ]
+  ];
 in
 {
 
@@ -10,25 +20,28 @@ in
   ];
 
   plugins = with pkgs.vimPlugins; [
-    # completion framework
+    # Utility functions for lsp
+    plenary-nvim
+    # Completion
     cmp-nvim-lsp
     nvim-cmp
     cmp-buffer
-    # lsp things
-    lsp_signature-nvim
-    lspkind-nvim
     nvim-lspconfig
-    # utility functions for lsp
-    plenary-nvim
-    # popout for documentation
-    popup-nvim
-    # snippets lists
+    # Lsp autocompletion thing on the right
+    lspkind-nvim
+    # Snippets
+    luasnip
+    cmp_luasnip
     friendly-snippets
-    # for showing lsp progress
+    # Function signature
+    lsp_signature-nvim
+    # Show LSP progress
     fidget-nvim
   ];
 
-  setup.fidget = { };
+  setup.fidget = {
+    progress.display.progress_ttl = 3;
+  };
 
   setup.lsp_signature = {
     bind = true;
@@ -37,30 +50,64 @@ in
     handler_opts.border = "single";
   };
 
+  use."luasnip.loaders.from_vscode".lazy_load = callWith {};
+
   use.lspkind.init = callWith {
     # defines how annotations are shown
     # default: symbol
     # options: "text", "text_symbol", "symbol_text", "symbol"
     mode = "symbol_text";
   };
+
   setup.cmp = {
+    snippet.expand = dsl.rawLua "function(args) require('luasnip').lsp_expand(args.body) end";
+    window = {
+      completion = {
+        winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None";
+        scrollbar = false;
+      };
+      documentation = {
+        border = border "CmpDocBorder";
+        winhighlight = "Normal:CmpDoc";
+      };
+    };
+
     mapping = {
-      "['<C-n>']" = rawLua "cmp.mapping.select_next_item({ behavior = require('cmp').SelectBehavior.Insert })";
-      "['<Down>']" = rawLua "cmp.mapping.select_next_item({ behavior = require('cmp').SelectBehavior.Select })";
-      "['<Tab>']" = rawLua "cmp.mapping.select_next_item({ behavior = require('cmp').SelectBehavior.Select })";
-
-      "['<C-p>']" = rawLua "cmp.mapping.select_prev_item({ behavior = require('cmp').SelectBehavior.Insert })";
-      "['<Up>']" = rawLua "cmp.mapping.select_prev_item({ behavior = require('cmp').SelectBehavior.Select })";
-      "['<S-Tab>']" = rawLua "cmp.mapping.select_prev_item({ behavior = require('cmp').SelectBehavior.Select })";
-
+      "['<C-n>']" = rawLua "cmp.mapping.select_next_item()";
+      "['<Down>']" = rawLua "cmp.mapping.select_next_item()";
+      "['<C-p>']" = rawLua "cmp.mapping.select_prev_item()";
+      "['<Up>']" = rawLua "cmp.mapping.select_prev_item()";
       "['<C-d>']" = rawLua "cmp.mapping.scroll_docs(-4)";
       "['<C-f>']" = rawLua "cmp.mapping.scroll_docs(4)";
       "['<C-Space>']" = rawLua "cmp.mapping.complete()";
       "['<C-e>']" = rawLua "cmp.mapping.close()";
-      "['<CR>']" = rawLua "cmp.mapping.confirm({ behavior = require('cmp').ConfirmBehavior.Replace, select = true, })";
+      "['<CR>']" = rawLua "cmp.mapping.confirm()";
+      "['<Tab>']" = rawLua ''
+        cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {"i", "s"})
+      '';
+      "['<S-Tab>']" = rawLua ''
+        cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif require("luasnip").jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          else
+            fallback()
+          end
+        end, { "i", "s" })
+    '';
     };
     sources = [
       { name = "nvim_lsp"; }
+      { name = "luasnip"; }
       { name = "buffer"; }
       { name = "crates"; }
       { name = "path"; }
@@ -86,9 +133,16 @@ in
     inherit capabilities;
   };
 
-  use.lspconfig.rnix.setup = callWith {
-    cmd = [ (getExe pkgs.rnix-lsp) ];
-    inherit capabilities;
+  use.lspconfig.nil_ls.setup = callWith {
+   cmd = [ (getExe pkgs.nil) ];
+   inherit capabilities;
+   settings = {
+     "['nil']".nix = {
+       maxMemoryMB = 8192;
+       flake.autoArchive = true;
+       flake.autoEvalInputs = true;
+     };
+   };
   };
 
   use.lspconfig.jsonls.setup = callWith {
