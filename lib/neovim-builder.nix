da26caae6c7776eedbc6514a0f57f1ccd6a759ed
Gytis Ivaskevicius <me@gytis.io>
2024-10-02 14:03:44 +0300

    feat: add specialArgs.modules

--- lib/neovim-builder.nix
+++ lib/neovim-builder.nix
@@ -12,7 +12,19 @@ let
       ./lsp.options.nix
       config
     ];
-    specialArgs = { inherit pkgs dsl; };
+    specialArgs = {
+      inherit pkgs dsl;
+      modules = {
+        nvchad = ./modules/nvchad.nix;
+        essentials = ./modules/essentials.nix;
+        git = ./modules/git.nix;
+        lsp = ./modules/lsp.nix;
+        nvim-tree = ./modules/nvim-tree.nix;
+        treesitter = ./modules/treesitter.nix;
+        telescope = ./modules/telescope.nix;
+        which-key = ./modules/which-key.nix;
+      };
+    };
   };
 in
 result.config.drv
